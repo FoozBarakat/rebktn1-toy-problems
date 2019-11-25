724f790539b4936e773ebfe0172f2daecbc2ab96
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-11-25 10:09:42 +0100

    toy1

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,31 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+var firstNonRepeatedCharacter = function(string, count, found) {
+ var found = found || [];
+ var count = count || 0;
+ var holdChar = string[count];
+
+ for (var i = 0; i < string.length; i++) {
+ 	if (holdChar !== string[i]) {
+ 		found.push(true);
+ 	} else {
+ 		found.push(false);
+ 	}
+ }
+
+ if (found.length === string.length) {
+ 	for (var i = 0; i < found.length; i++) {
+ 		if (found[i] === true) {
+ 			return string[i];
+ 		}
+ 	}
+
+ }
+
+ return firstNonRepeatedCharacter(string, count++, found);
+
 };
+
+console.log( firstNonRepeatedCharacter('ABA') );
+console.log( firstNonRepeatedCharacter('AACBDB') );
