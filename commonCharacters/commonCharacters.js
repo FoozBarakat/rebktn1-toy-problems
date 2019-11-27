5529966723b19c14bdf6dff92b0c2400efa3ef77
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-11-27 09:46:51 +0100

    3

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -13,5 +13,53 @@
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  var hold = [];
+
+  for (var i = 0; i < string1.length; i++) {
+  	
+  	for (var j = 0; j < string2.length; j++) {
+  		
+  		if (string1[i] === string2[j]) {
+  			
+  			if ( !hold.includes(string1[i]) && string1[i] !== " ") {
+  				hold.push(string1[i]);	
+  			}
+  			
+  		}
+  	}
+  }
+
+  return hold.join("");
 };
+
+console.log( commonCharacters('acexiv ou', 'a egihobu') );
+
+// Extra credit
+
+/*var commonCharacters = function(string1, string2, string3) {
+  var hold = [];
+
+  if (string2.length > string3.length) {
+      var len = string2.length;
+  } else {
+      var len = string3.length;
+  }
+
+  for (var i = 0; i < string1.length; i++) {
+  	
+  	for (var j = 0; j < len; j++) {
+  		
+  		if (string1[i] === string2[j] && string1[i] === string3[j]) {
+  			
+  			if ( !hold.includes(string1[i]) && string1[i] !== " ") {
+  				hold.push(string1[i]);	
+  			}
+  			
+  		}
+  	}
+  }
+
+  return hold.join("");
+};
+
+console.log( commonCharacters('acefxiv ou', 'a egihobu', 'akjfsldf') ); */
