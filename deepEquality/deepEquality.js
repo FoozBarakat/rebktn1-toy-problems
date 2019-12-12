93b8279226a6024fb0f0286ebb8951556c1c1a91
FoozBarakat <reachfooz@gmail.com>
2019-12-12 09:29:50 +0100

    apple

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,29 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange) {
+ var deepEquals = function(apple, orange, result) {
+  var result = result || [];
+
+  for (var key in apple) {
+    
+    if ( apple[key] === orange[key] && typeof apple[key] !== 'object') {
+      result.push(true)
+    } else if ( apple[key] !== orange[key] && typeof apple[key] !== 'object'){
+      result.push(false);
+    }
+
+    if (typeof apple[key] === 'object') {
+        deepEquals(apple[key], orange[key], result);
+    }
+  }
+
+  if( result.includes(false) ) {
+      return false;
+  }
+
+  return true;
 };
+
+console.log( deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}) );
+console.log ( deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}) );
+console.log ( deepEquals({a:1, b: {c:5}, d:7},{a:1, b: {c:3}}) );
