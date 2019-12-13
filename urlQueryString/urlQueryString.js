fb5ff4c0a918daf89ea75042b5e7d9a40126acf5
FoozBarakat <reachfooz@gmail.com>
2019-12-13 09:43:30 +0100

    0

--- urlQueryString/urlQueryString.js
+++ urlQueryString/urlQueryString.js
@@ -1,6 +1,7 @@
 /*
 URL Query String
-Given a properly formatted URL, return the query string data as an array of key-value pairs, in the order that they appear.
+Given a properly formatted URL, return the query string data as an array of key-value pairs, 
+in the order that they appear.
 
 HINT: You may use decodeURIComponent()
 
@@ -9,4 +10,41 @@ parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello wo
 parseQueryString("http://example.com") // undefined
 */
 
-function parseQueryString(url) {}
+function parseQueryString(url) {
+    var result = [];
+    var char = '';
+    var word = '';
+
+    if ( !url.includes('?') ) { return undefined};
+
+    index = url.indexOf('?');
+
+    for (var i = index + 1; i < url.length; i++) {
+        if (url[i] === '='){
+            result.push(char);
+            char = '';
+
+            for (var j = i + 1; j < url.length; j++) {
+                if(url[j] === '&') {
+                    result.push(word);
+                    word = '';
+                    i = j+1;
+                    break;
+                }
+                word += url[j];
+            }
+        }
+        char += url[i];
+    }
+
+    return result;
+}
+
+// [ [ "a", "hello" ], [ "b", "99" ] ]
+console.log( parseQueryString("http://example.com?a=hello&b=99") );
+
+//[ [ "msg", "hello world" ] ]
+console.log( parseQueryString("http://example.com?msg=hello%20world") );
+
+//undefined
+console.log( parseQueryString("http://example.com") );
