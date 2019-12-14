8c3763c6dda659acb5263cf0704f11955b5bc566
FoozBarakat <reachfooz@gmail.com>
2019-12-14 09:21:44 +0100

    z

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -1,10 +1,39 @@
 /*
 Largest Product of Three
-Write a function that accepts an array of integers and returns the largest product possible from three of those numbers.
+Write a function that accepts an array of integers and returns the largest product possible 
+from three of those numbers.
 
 largestProductOfThree([ 2, 1, 3, 7 ]) // 42
 largestProductOfThree([ 0, 2, 3 ]) //	0
 largestProductOfThree([1, 4, -5, -2]) // 40
 */
 
-function largestProductOfThree(array) {}
+function largestProductOfThree(array) {
+    if (array.length < 3) {return 'the array length is less than 3'};
+    var maxArr = [];
+    var result = 1;
+
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            array[i] *= -1;
+        }
+    }
+
+    for (var i = 0; i < 3; i++) {
+     var max = Math.max(...array);
+     maxArr.push(max);
+     var index = array.indexOf(max);
+     array.splice(index, 1);
+    }
+
+    for (var i = 0; i < maxArr.length; i++) {
+        result *= maxArr[i];
+    }
+    
+    return result;
+    
+}
+
+console.log( largestProductOfThree([ 2, 1, 3, 7 ]) );
+console.log( largestProductOfThree([ 0, 2, 3 ]) );
+console.log( largestProductOfThree([1, 4, -5, -2]) );
