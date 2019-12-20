789d74a1175f69b8e0520153e9dc7a1787b11109
FoozBarakat <reachfooz@gmail.com>
2019-12-20 09:12:10 +0100

    20

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,19 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+  var counter = 0;
+
+  for (var i = 0; i < str.length; i++) {
+
+    if( str[i] === '(' ) { counter++; } 
+    if( str[i] === ')') { counter--; }
+  }
+
+  return counter === 0;
+};
+
+console.log( isBalanced("(x + y) - (4)") );
+console.log( isBalanced("(((10 ) ()) ((?)(:)))") );
+console.log( isBalanced("(50)(") );
+console.log ( isBalanced("") );
