9d72634da7e5ccd6ea178bb5190b9f07003a2061
FoozBarakat <reachfooz@gmail.com>
2019-12-24 09:20:59 +0100

    24

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -9,4 +9,36 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+  var arr = [];
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === ' {') {
+      arr.push(str[i]);
+    } 
+    if (str[i] === ')') {
+      var index = arr.indexOf('(');
+      arr.splice(index, 1);
+    }
+    if (str[i] === ']') {
+      var index = arr.indexOf('[');
+      arr.splice(index, 1);
+    }
+    if (str[i] === '}') {
+      var index = arr.indexOf('{');
+      arr.splice(index, 1);
+    }
+  };
+
+  if (arr.length) {
+    return false;
+  } 
+  
+  return true;
+};
+
+console.log( isBalanced("(x + y) - (4)") ); // true
+console.log( isBalanced("(((10 ) ()) ((?)(:)))") ); // true
+console.log( isBalanced("[{()}]") ); // true
+console.log( isBalanced("(50)(")  ); // false
+console.log( isBalanced("[{]}") ); // false
