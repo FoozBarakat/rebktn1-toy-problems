76adea8b65ce7899391b7a52c7618ccc5a85adaa
FoozBarakat <reachfooz@gmail.com>
2019-12-26 09:44:12 +0100

    26

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,25 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+ var allAnagrams = function(string) { 
+  var holdStr = '';
+  return helper(holdStr, string.split(''), string);
 };
+
+var helper = function(holdStr, arr, string, result) {
+  var result = result || [];
+  if (holdStr === string.length - 1) {
+        result.push(holdStr);
+        console.log(result)
+  } else {
+    for (var i = 0; i < arr.length; i++){
+      holdStr += arr[i];
+      console.log(holdStr)
+      arr.splice(i, 1);
+      console.log(arr)
+      helper(holdStr, arr, string, result)
+    }
+  } 
+}
+
+console.log(allAnagrams('abc'))
