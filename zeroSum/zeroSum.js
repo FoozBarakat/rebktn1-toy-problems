2d45349884d51cf10eb98b58ecae667fa9b7b340
FoozBarakat <reachfooz@gmail.com>
2019-12-31 09:31:21 +0100

    31

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,53 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+
+// linear 
+var zeroSum = (array, count) => {
+  var positive = {};
+  var negitive = {};
+  
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      positive[array[i]] = array[i]; 
+    } else {
+      negitive[ Math.abs(array[i]) ] = array[i];
+    }
+  }
+  
+  for (var key in negitive) {
+    if (positive[key] !== undefined) {
+      return true;
+    } 
+  }
+  
+  return false;
+  }
+  
+  console.log( zeroSum([ 1, 3, 2, -3 ]) ); // true
+  console.log( zeroSum([ 5, 7, 2, 9 ]) ); // false
+
+// not linear
+var zeroSum = (array, count) => {
+
+  var count = count || 0;
+  
+  for (var i = 0; i < array.length; i++) {
+    //console.log(Math.abs(array[count]), Math.abs(array[i+1]))
+    if( (count !== i) && (Math.abs(array[count]) === Math.abs(array[i])) ) {
+      return true;
+    };
+  }
+  
+  if(count === array.length) {
+    return false;
+  }
+  
+  count++;
+  
+  return zeroSum(array, count);
+  
+  }
+  
+  //console.log( zeroSum([ 1, 3, 2, -3 ]) ); // true
+  //console.log( zeroSum([ 5, 7, 2, 9 ]) ); // false
