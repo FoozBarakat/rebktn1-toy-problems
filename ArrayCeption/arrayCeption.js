032908fa95ac66f468f1a44c87a7ef7b555d6ccd
FoozBarakat <reachfooz@gmail.com>
2020-01-14 09:55:44 +0100

    14/1

--- ArrayCeption/arrayCeption.js
+++ ArrayCeption/arrayCeption.js
@@ -15,3 +15,27 @@
 //  [ ] ==>	0
 //  array:
 //  [ [ [ ] ] ] ==>	0
+
+var arrayception = (array, count) => {
+  var count = count || 0;
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (Array.isArray(array[i])) {
+      arrayception(array[i], count);
+      count++;
+    } else if (array[i].length === 0) {
+      count = 0;
+    } else {
+      count = 1;
+    }
+  }
+
+  return count;
+}
+
+console.log(arrayception([[5], [[]]]), '2');
+console.log(arrayception([10, 20, 30, 40]), '1');
+console.log(arrayception([[10, 20], [[30, [40]]]]), '4');
+console.log(arrayception([]), '0');
+console.log(arrayception([[[]]]), '0');
