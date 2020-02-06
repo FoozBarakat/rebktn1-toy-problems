7d4afb9e311716b84c704e3d664d0edf1b319dd0
FoozBarakat <reachfooz@gmail.com>
2020-02-06 09:31:53 +0100

    done

--- militaryTime/militaryTime.js
+++ militaryTime/militaryTime.js
@@ -10,6 +10,45 @@ toMilitary("12:00am") // "00:00"
 toMilitary("04:00") // "04:00"
 */
 
+var obj = {
+  '1': '13',
+  '2': '14',
+  '3': '15',
+  '4': '16',
+  '5': '17',
+  '6': '18',
+  '7': '19',
+  '8': '20',
+  '9': '21',
+  '10': '22',
+  '11': '23',
+  '12': '00'
+};
+
 function toMilitary(time) {
-  // your code here...
-}
+  var result = '';
+
+  if (time.includes('am')) {
+    var x = time.slice(0, 2);
+    time = time.replace('am', '');
+
+    if (x === '12') {
+      result = time.slice(2);
+      result = obj[x] + result;
+      return result
+    }
+    return time;
+  } else if (time.includes('pm')) {
+    time = time.replace('pm', '');
+    result = time.slice(1);
+    result = obj[time[0]] + result;
+    return result;
+  } else {
+    return time;
+  };
+};
+
+console.log(toMilitary("3:00pm"), 'result: 15:00');
+console.log(toMilitary("9:15am"), 'result: 09:15');
+console.log(toMilitary("12:00am"), 'result: 00:00');
+console.log(toMilitary("04:00"), 'result: 04:00');
